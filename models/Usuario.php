3b0c9fccd13bd589c224557586a49d1c2d18c8fe
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-10-23 18:57:04 -0500

    feat: Valida el email para el restablecimiento de contraseña

--- models/Usuario.php
+++ models/Usuario.php
@@ -19,32 +19,47 @@ class Usuario extends ActiveRecord
   }
 
   // Validacion para cuentas nuevas
-  public function validarNuevaCuenta() {
-    if(!$this->nombre) {
+  public function validarNuevaCuenta()
+  {
+    if (!$this->nombre) {
       self::$alertas['error'][] = 'El nombre del Usuario es obligatorio';
     }
-    if(!$this->email) {
+    if (!$this->email) {
       self::$alertas['error'][] = 'El email es obligatorio';
     }
-    if(!$this->password) {
+    if (!$this->password) {
       self::$alertas['error'][] = 'El password no puede ir vacío';
     }
-    if(strlen($this->password) < 6) {
+    if (strlen($this->password) < 6) {
       self::$alertas['error'][] = 'El password debe tener al menos 6 caracteres';
     }
-    if($this->password !== $this->password2) {
+    if ($this->password !== $this->password2) {
       self::$alertas['error'][] = 'Los passwords no coinciden';
     }
     return self::$alertas;
   }
 
+  // Valida un email
+  public function ValidarEmail()
+  {
+    if (!$this->email) {
+      self::$alertas['error'][] = 'El email es obligatorio';
+    }
+    if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+      self::$alertas['error'][] = 'Email no válido';
+    }
+    return self::$alertas;
+  }
+
   // Hashear el password
-  public function hashPassword() {
+  public function hashPassword()
+  {
     $this->password = password_hash($this->password, PASSWORD_BCRYPT);
   }
 
   // Generar un token único
-  public function crearToken() {
+  public function crearToken()
+  {
     $this->token = uniqid();
   }
 }
