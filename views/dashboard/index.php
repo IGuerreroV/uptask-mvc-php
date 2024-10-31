6424f9dce957ed4de66311c9f24b8415593cc315
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-10-30 21:36:13 -0500

    ui: Listando todos los proyectos del usuario

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,5 +1,17 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
-
+<?php if (count($proyectos) === 0): ?>
+  <p class="no-proyectos">No Hay Proyectos aún <a href="/crear-proyecto">Comienza creando uno</a></p>
+<?php else: ?>
+  <ul class="listado-proyectos">
+    <?php foreach ($proyectos as $proyecto): ?>
+      <li class="proyecto">
+        <a href="/proyecto?url=<?php echo $proyecto->url; ?>">
+          <?php echo $proyecto->proyecto; ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
