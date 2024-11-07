0f0db766155009e0ccf9dc5d3cd61efa0bf7bcbc
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-11-07 16:27:03 -0500

    feat: Creando Routing y Controladores para Tareas
    - Añadido el controlador TareaController con métodos para crear, actualizar y eliminar tareas.
    - Configurado el routing para manejar las solicitudes POST para crear, actualizar y eliminar tareas.

---
+++ controllers/TareaController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Controllers;
+
+use Model\ActiveRecord;
+
+class TareaController
+{
+  public static function index()
+  {
+
+  }
+
+  public static function crear()
+  {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+  }
+
+  public static function actualizar()
+  {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+      
+    }
+  }
+
+  public static function eliminar()
+  {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+      
+    }
+  }
+}
