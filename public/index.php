00d096b0c6d5dadf3bca8ec51e653fe7568498ff
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-11-06 20:28:11 -0500

    feat: Creando el modelo de Tareas
    - Añadido el modelo de Tareas con los atributos id, nombre, estado, proyecto_id.
    - Implementadas las relaciones con el modelo de Proyecto.

--- public/index.php
+++ public/index.php
@@ -36,5 +36,7 @@ $router->post('/crear-proyecto', [DashboardController::class, 'crear_proyecto'])
 $router->get('/proyecto', [DashboardController::class, 'proyecto']);
 $router->get('/perfil', [DashboardController::class, 'perfil']);
 
+// API para tareas
+
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
